d8058541a3a5d1b0bee017b06b5d329c8cc7c654
asdmg <alex.sdmg@hotmail.com>
2025-12-11 17:03:17 +0000

    frase modificada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Seu futuro agradece cada minuto dedicado agora.</h1>
+    return <h1>Seu futuro agradece cada minuto dedicado agora, e valerá a pena no final.</h1>
 }
 
 export default Home;
